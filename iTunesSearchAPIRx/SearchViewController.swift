3ba42f5c3038f50ff1ac0c78423eeeee2fcdaa71
한 <hih458@naver.com>
2023-11-08 15:09:51 +0900

    feat Main Storyboard 삭제

--- iTunesSearchAPIRx/SearchViewController.swift
+++ iTunesSearchAPIRx/SearchViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
+    
+    
 }
 
