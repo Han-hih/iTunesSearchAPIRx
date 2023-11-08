a727ddf7a6fa9ee0b23719ab3cf0ef7269b7700f
한 <hih458@naver.com>
2023-11-08 17:35:39 +0900

    add searchBar 생성

--- iTunesSearchAPIRx/ViewControllers/SearchViewController.swift
+++ iTunesSearchAPIRx/ViewControllers/SearchViewController.swift
@@ -7,15 +7,45 @@
 
 import UIKit
 
+import SnapKit
+
 final class SearchViewController: UIViewController {
 
-    let viewModel = SearchViewModel()
+   private let searchBar = {
+        let search = UISearchBar()
+        search.placeholder = "게임, 앱, 스토리 등"
+        search.setShowsCancelButton(true, animated: true)
+        return search
+    }()
     
+    
+    
+   private let viewModel = SearchViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationSetting()
+        setUI()
     }
+    
+    
+   private func navigationSetting() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "검색"
+    }
+    
+   private func setUI() {
+        [searchBar].forEach {
+            view.addSubview($0)
+        }
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        
+    }
+    
     
 }
 
