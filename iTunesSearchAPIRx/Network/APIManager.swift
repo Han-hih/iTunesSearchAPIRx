ed275ef8326f38bb1ef7f1679acc12bb8799081a
한 <hih458@naver.com>
2023-11-08 17:12:26 +0900

    feat itunesAPI받아오는 URLSession생성
    - NetworkModel 생성
    - API 불러오는 함수 Observable로 생성

--- iTunesSearchAPIRx/Network/APIManager.swift
+++ iTunesSearchAPIRx/Network/APIManager.swift
@@ -5,11 +5,44 @@
 //  Created by 황인호 on 11/8/23.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+private enum APIError: Error {
+    case invalidURL
+    case unknownResponse
+    case statusError
+}
 
 class APIManager {
-    
-    
-    
-    
+    static func fetchitunesData(term: String) -> Observable<Search> {
+        
+        return Observable<Search>.create { observer in
+            
+            guard let url = URL(string: "https://itunes.apple.com/search?term=\(term)&country=kr&media=software") else {
+                observer.onError(APIError.invalidURL)
+                return Disposables.create()
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                print("DataTask Succeed")
+                if let _ = error {
+                    observer.onError(APIError.unknownResponse)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                    observer.onError(APIError.statusError)
+                    return
+                }
+                
+                if let data = data, let appData = try? JSONDecoder().decode(Search.self, from: data) {
+                    observer.onNext(appData)
+                }
+            }.resume()
+            
+            return Disposables.create()
+        }
+    }
 }
---
+++ iTunesSearchAPIRx/Network/NetworkModel.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkModel.swift
+//  iTunesSearchAPIRx
+//
+//  Created by 황인호 on 11/8/23.
+//
+
+import Foundation
+
+struct Search: Decodable {
+    let resultCount: Int
+    let results: [results]
+}
+
+struct results: Decodable {
+    let artworkUrl512: [String] //앱 아이콘
+    let screenshotUrls: [String] // 스크린샷
+    let trackName: String // 앱 이름
+    let artistName: String // 앱 제작사 이름
+    let releaseNotes: String // 업데이트 내용
+    let description: String // 앱 설명
+    let version: String // 현재 버전
+    let averageUserRating: Double // 평점
+}
